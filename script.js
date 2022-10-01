ede40d207a985ba1c431cabfac13ee5b3b7614db
UrraFacundo <103340927+UrraFacundo@users.noreply.github.com>
2022-10-01 19:37:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//#region variables
+let num1 = 10;
+let num2 = 15;
+const num3 = 5;
+//#endregion
+
+//#region calculos
+let suma = num1 + num2 + num3;
+let resta = num1 - num2 - num3;
+let division = num2 / num3 / num1;
+let multiplicacion = num3 * num2 * num1;
+//#endregion
+
+//#region array
+let inicio = 0;
+let fin = [3];
+
+for (inicio of fin){
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+}
+//#endregion
+
+//#region prog. principal
+console.log(suma);
+console.log(resta);
+console.log(division);
+console.log(multiplicacion);
+//#endregion
